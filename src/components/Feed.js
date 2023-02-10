083540cc9108f5c397519412083ad84a3b08d466
Anurag-Bharati <anuragbharati26@gmail.com>
2023-02-10 16:16:01 +0545

    Implemented public feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,13 +1,50 @@
-import {
-    BiRefresh,
-    BiMessageAltDots,
-    BiHeart,
-    BiTrash,
-    BiDotsHorizontalRounded,
-} from "react-icons/bi";
-import Moment from "react-moment";
+import axios from "@/services/axios";
+import { useEffect, useId, useState } from "react";
+import { BiRefresh } from "react-icons/bi";
+import { AnimatePresence, motion } from "framer-motion";
 import Post from "./Post";
 export default function Feed() {
+    const [posts, setPosts] = useState([]);
+    const [stories, setStories] = useState([]);
+    const [page, setPage] = useState(2);
+    // for SSR + CSH Rendering strategy
+    const reactId = useId();
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3000/api/v0/post?page=1`)
+            .then((x) => setPosts(x.data["data"]["feed"]))
+            .catch((err) => console.log(err));
+        axios
+            .get(`http://localhost:3000/api/v0/story`)
+            .then((x) => setStories(x.data["data"]))
+            .catch((err) => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        function handleScroll() {
+            const { scrollTop, scrollHeight, clientHeight } =
+                document.documentElement;
+            if (scrollTop + clientHeight >= scrollHeight - 10) {
+                increasePage();
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [page]);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3000/api/v0/post?page=${page}`)
+            .then((x) => setPosts((old) => old.concat(x.data["data"]["feed"])))
+            .catch((err) => console.log(err));
+    }, [page]);
+
+    function increasePage() {
+        setPage((x) => x + 1);
+    }
+
     return (
         <div className="xl:ml-[370px] xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
             <div className="flex py-2 px-3 sticky top-0 z-50 bg-rn-black">
@@ -18,9 +55,27 @@ export default function Feed() {
                     <BiRefresh className="h-6 w-6" />
                 </div>
             </div>
-            <Post></Post>
-            <Post></Post>
-            <Post></Post>
+
+            {posts.length === 0 ? (
+                <div className="progress-bar" />
+            ) : (
+                <AnimatePresence>
+                    {posts.map((post, i) => (
+                        <motion.div
+                            key={reactId + `${i}:`}
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                duration: 0.3,
+                                delay: i * 0.1 > 0.3 ? 0.3 : i * 0.1,
+                            }}
+                            whileTap={{ scale: 0.95 }}
+                        >
+                            <Post key={post._id} id={post._id} post={post} />
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
+            )}
         </div>
     );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,10 @@
 import {
-    BiMessageAltDots,
+    BiMessageSquareDots,
     BiHeart,
     BiTrash,
     BiDotsHorizontalRounded,
 } from "react-icons/bi";
+import { BsHeart } from "react-icons/bs";
 import Moment from "react-moment";
 import { useState, useEffect, useId } from "react";
 import { useRouter } from "next/router";
@@ -13,7 +14,7 @@ export default function Post({ post, id }) {
     const [likes, setLikes] = useState([]);
     const [comments, setComments] = useState([]);
     const [hasLiked, setHasLiked] = useState(false);
-    const currentUser = {};
+    const currentUser = null;
     // for SSR + CSH Rendering strategy
     const reactId = useId();
     useEffect(
@@ -61,7 +62,8 @@ export default function Post({ post, id }) {
             router.push("/");
         }
     }
-
+    const numImages = post.mediaFiles.length;
+    const gridCols = numImages === 2 ? 2 : numImages > 2 ? 3 : 1;
     return (
         <div className="flex-1 pt-3 pl-3 pr-3 cursor-pointer bg-white10 rounded-2xl m-2">
             {/* Header */}
@@ -70,21 +72,21 @@ export default function Post({ post, id }) {
                 <div className="flex items-center space-x-1 whitespace-nowrap">
                     <img
                         className="h-11 w-11 rounded-full mr-2"
-                        src={null}
+                        src={`http://localhost:3000/${post?.owner.avatar}`}
                         alt="user-img"
                     />
                     <div className="flex flex-col space-y-0 ">
                         <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-                            Anurag Bharati
+                            {post?.owner.fname + " " + post?.owner.lname}
                         </h4>
                         <span className="text-sm sm:text-[15px]">
-                            @anuragbharati
+                            @{post?.owner.username}
                         </span>
                     </div>
                 </div>
                 <div className="flex items-center">
                     <span className="text-sm sm:text-[15px] hover:underline">
-                        <Moment fromNow>{Date.now()}</Moment>
+                        <Moment fromNow>{post?.createdAt}</Moment>
                     </span>
                     {/* dot icon */}
                     <BiDotsHorizontalRounded className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
@@ -92,34 +94,71 @@ export default function Post({ post, id }) {
             </div>
             {/* post text */}
 
-            <p onClick={null} className=" text-[15px sm:text-[16px] mb-2">
-                So awesome...
+            <p
+                onClick={() => router.push(`/posts/${id}`)}
+                className=" text-[15px sm:text-[16px] mb-2"
+            >
+                {post?.caption}
             </p>
 
-            <div className={`grid gap-2 grid-cols-1`}>
-                <img src="https://media.pocketgamer.com/artwork/na-28660-1675375803/brawl-stars-edgar-guide-header.jpg" />
+            <div className={`grid gap-2 grid-cols-${gridCols}`}>
+                {post.mediaFiles.map((media, i) => (
+                    <img
+                        key={reactId + `${i}:`}
+                        onClick={() => router.push(`/posts/${id}`)}
+                        className="rounded-lg w-full max-h-80 object-cover overflow-hidden"
+                        src={`http://localhost:3000/${media?.url}`}
+                        alt="post-image"
+                    />
+                ))}
             </div>
             {/* icons */}
             <div className="flex justify-left pt-2 pb-2 space-x-4">
                 <div className="flex items-center">
-                    <BiHeart
+                    {hasLiked ? (
+                        <BsHeart
+                            onClick={likePost}
+                            className="h-9 w-9 hoverEffect p-2 text-[#ef476fe6] hover:bg-red-100"
+                        />
+                    ) : (
+                        <BiHeart
+                            onClick={likePost}
+                            className="h-9 w-9 hoverEffect p-2 hover:text-[#ef476fe6] hover:bg-red-100"
+                        />
+                    )}
+                    {likes.length > 0 && (
+                        <span
+                            className={`${
+                                hasLiked && "text-[#ef476fe6]"
+                            } text-sm select-none`}
+                        >
+                            {" "}
+                            {likes.length}
+                        </span>
+                    )}
+                </div>
+                <div className="flex items-center select-none ">
+                    <BiMessageSquareDots
+                        onClick={() => {
+                            if (!currentUser) {
+                                // signIn();
+                                router.push("/auth/signin");
+                            } else {
+                                // comment page
+                            }
+                        }}
+                        className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
+                    />
+                    {comments.length > 0 && (
+                        <span className="text-sm">{comments.length}</span>
+                    )}
+                </div>
+                {currentUser?._id === post?.owner._id && (
+                    <BiTrash
                         onClick={null}
                         className="h-9 w-9 hoverEffect p-2 hover:text-[#ef476fe6] hover:bg-red-100"
                     />
-                    <span className={"text-[#ef476fe6]"}> {100}</span>
-                </div>
-                <div className="flex items-center select-none ">
-                    <BiMessageAltDots
-                        onClick={null}
-                        className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
-                    />
-                    <span className="text-sm">{100}</span>
-                </div>
-
-                <BiTrash
-                    onClick={null}
-                    className="h-9 w-9 hoverEffect p-2 hover:text-[#ef476fe6] hover:bg-red-100"
-                />
+                )}
             </div>
         </div>
     );
