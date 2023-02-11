7ddf445ad603a53cef2234f1229f9b1eccc28906
Anurag-Bharati <anuragbharati26@gmail.com>
2023-02-12 01:34:12 +0545

    Implemented mutual post section w/ state management. Added animations and Session Persistance

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -5,3 +5,11 @@ export const fetchPublicFeed = async ({ page, refresh }) => {
         `http://localhost:3000/api/v0/post?page=${page}&refresh=${refresh}`
     );
 };
+
+export const fetchMutualFeed = async ({ page, refresh }) => {
+    const token = localStorage.getItem("token");
+    return axios.get(
+        `http://localhost:3000/api/v0/post/feed?page=${page}&refresh=${refresh}`,
+        { headers: { Authorization: token } }
+    );
+};
