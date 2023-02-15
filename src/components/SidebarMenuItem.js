bf792d9a6549e146f2952b853bfa17a3895f73ed
Anurag-Bharati <anuragbharati26@gmail.com>
2023-02-15 09:21:31 +0545

    Added comment component w/ proper doc. Fixed an issue with like state on public feed scope.

--- src/components/SidebarMenuItem.js
+++ src/components/SidebarMenuItem.js
@@ -1,8 +1,13 @@
 export default function SidebarMenuItem({ text, Icon, active }) {
     return (
-        <div className="hoverEffect flex items-center justify-center xl:justify-start text-lg space-x-3 my-3">
+        <div
+            className={`${
+                active &&
+                "text-black  bg-gray-200  rounded-full w-[52px] xl:w-auto h-[52px] xl:h-auto xl:p-3 "
+            } hoverEffect flex items-center justify-center xl:justify-start text-lg space-x-3 my-3`}
+        >
             <Icon className="h-7 w-7" />
-            <span className={`${active && "font-bold"} hidden xl:inline`}>
+            <span className={`${active && "font-bold "} hidden xl:inline`}>
                 {text}
             </span>
         </div>
