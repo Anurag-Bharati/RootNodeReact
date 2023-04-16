cc5a2243c321db0913ac0400c202045c09d33007
Anurag-Bharati <anuragbharati26@gmail.com>
2023-04-16 21:23:49 +0545

    Hotfix: Fix for i18n support is not compatible with next export

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {},
-    i18n: {
-        locales: ["en-US"],
-        defaultLocale: "en-US",
-    },
     reactStrictMode: true,
     images: {
         domains: ["res.cloudinary.com"],
-        minimumCacheTTL: 60,
     },
 };
 
